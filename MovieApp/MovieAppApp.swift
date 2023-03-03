06bcf2f159ecd0b7b073084cb23aa16b40ad3cef
1lugesya <sariaysegulll@gmail.com>
2023-03-03 23:12:10 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ayşegül Sarı on 3.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
